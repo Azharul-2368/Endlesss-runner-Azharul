b1830f37baf97bd8d770a9b9da31e3c210a6233d
Azharul-2368 <azharul2368islam@gmail.com>
2024-10-01 09:48:15 +0100

    coin colllectible

---
+++ Assets/CoinCollect.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class CoinCollect : MonoBehaviour
+{
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        // Check if the object collidiing with the coin is the player
+        if (other.gameObject.CompareTag("player")) ;
+        {
+            // Add ay score incrementing logic here, e.g.;
+            // ScoreManager.instance.Addscore(1);
+
+            //Destroy the coin when it hits the player
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/CoinSpawner.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinSpawner : MonoBehaviour
+{
+    //Reference to the coin prefab
+    public GameObject coinPrefab;
+
+    //define the offset or range where the coin shouls spawn on the platform.
+    public Vector2 coinOffset = new Vector2(0, 1); //Adjust as necessary
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //spawn the coin on the platform at the desired position
+        SpawnCoin();
+
+    }
+
+    // Update is called once per frame
+    void SpawnCoin()
+    {
+        //calculate the coin position relative to the platform
+        Vector3 coinPosition = transform.position + (Vector3)coinOffset;
+
+        //Instantiate the coin at the calculated position
+        Instantiate(coinPrefab, coinPosition, Quaternion.identity);
+
+
+    }
+}
