ebb84a27ecf0a6901de08ce2da59f29841ba6f7e
Azharul-2368 <azharul2368islam@gmail.com>
2024-10-08 12:43:25 +0100

    attempt at coin audio

--- Assets/CoinCollect.cs
+++ Assets/CoinCollect.cs
@@ -2,12 +2,31 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using System.Collections;
 
 public class CoinCollect : MonoBehaviour
 {
-    
+    public AudioClip Coin;
+    AudioSource aSource;
+    GameObject obj;
+
+    void start()
+    {
+        obj = GameObject.Find("AudioObject");
+        if (obj != null)
+        aSource = obj.GetComponent<AudioSource>();
+    }
+
+
+
     void OnTriggerEnter2D(Collider2D other)
     {
+        if (other.tag == "player")
+        {
+            aSource.clip = Coin;
+            aSource.Play();
+        }
+
         // Check if the object collidiing with the coin is the player
         if (other.gameObject.CompareTag("player")) ;
         {
@@ -17,6 +36,7 @@ public class CoinCollect : MonoBehaviour
             //Destroy the coin when it hits the player
             Destroy(gameObject);
             ScoreManager.instance.AddPoint();
+            
         }
     }
 }
